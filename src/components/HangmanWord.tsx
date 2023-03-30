0f3f097365fcbb1de4fd26d9e2c8f2e717f11713
Etgoz <etgoz151@gmail.com>
2023-03-30 10:20:27 +0300

    HangmanWord step 1

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export function HangmanWord() {
-	return <div style={{ display: 'flex', gap: '.25em', fontSize: '6rem' }}></div>;
+	const word = 'test';
+	const guessedLetters = ['t'];
+	return (
+		<div
+			style={{
+				display: 'flex',
+				gap: '.25em',
+				fontSize: '6rem',
+				fontWeight: 'bold',
+				textTransform: 'uppercase',
+				fontFamily: 'monospace',
+			}}
+		>
+			{word.split('').map((letter, index) => (
+				<span style={{ borderBottom: '.1em solid black' }} key={index}>
+					<span style={{ visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden' }}>{letter}</span>
+				</span>
+			))}
+		</div>
+	);
 }
